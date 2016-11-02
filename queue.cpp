f7d27f91bc44eda366727bda315f1237f921c340
mayazach <mayazaha2@gmail.com>
2016-11-02 14:36:32 +0200

    Update queue.cpp

--- queue.cpp
+++ queue.cpp
@@ -1,77 +1,59 @@
+
 #include <iostream>
+#include "queue.h"
+
 using namespace std;
 
-class Queue{
-
-struct Node{
-	int s;
-	Node *next;
-	Node(int num,Node *point=NULL):s(num),next(point){
-		cout << "Creating new node." << endl;}
-};
-
-	Node *head;
-	int size;
-public:
-	Queue():head(NULL),size(0){cout << "Queue was just created." << endl;}
-	~Queue(){
-		while(head != NULL){
-			Node *n = head->next;
-			delete head;
-			head = n;
-		}
-		cout << "Queue destroyed." << endl;
+Queue::~Queue(){
+	while(head != NULL){
+		QueueNode *n = head->next;
+		delete head;
+		head = n;
 	}
-	int getSize(){return size;}
-	bool isEmpty(){
-		if(head==NULL)
+	cout << "Queue destroyed." << endl;
+}
+
+void Queue::push(int id,int father){
+	QueueNode **n=&head;
+	while(*n != NULL)
+		n=&((*n)->next);
+	*n= new QueueNode(id,father);
+	size++;
+}
+
+void Queue::print(){
+	QueueNode *n=head;
+	while(n!=NULL){
+		cout << n->data.id << " child of " << n->data.father << ",";
+		n=n->next;
+	}
+	cout << endl;
+}
+
+int Queue::pop(){
+	QueueNode *n;
+	n=head;
+	head=head->next;
+	size--;
+	return n->data.id;
+}
+
+bool Queue::member(int num){
+	QueueNode *n=head;
+	while(n!=NULL){
+		if(n->data.id == num)
 			return true;
-		else
-			return false;
-	}
-	void pushBack(int num){
-	//add element num to the end of the queue
-		Node **n=&head;
-		while(*n != NULL)
-			n=&((*n)->next);
-		*n= new Node(num);
-		size++;
-	}
-	void print(){
-	//print all elements
-		Node *n=head;
-		while(n!=NULL){
-			cout << n->s << " ";
-			n=n->next;
+		n=n->next;
 		}
-		cout << endl;
-	}
-	int pop(){
-	//remove first element from queue and return
-		Node *n;
-		n=head;
-		head=head->next;
-		size--;
-		return n->s;
-	}
-	bool member(int num){
-	//check if num is already in the queue
-		Node *n=head;
-		while(n!=NULL){
-			if(n->s == num)
-				return true;
-			n=n->next;
-		}
-		return false;
-	}
-	int front(){if(head!=NULL)return head->s;}//return first element
-	int back(){
-	//return last element
-		Node *n=head;
-		if(head!=NULL){
-			while(n->next!=NULL)
-				n=n->next;
-			return n->s;
-		}
-	}
-};
+	return false;
+}
+
+int Queue::getFather(int num){
+	QueueNode *n=head;
+	while(n!=NULL){
+                if(n->data.id == num)
+                        return n->data.father;
+                n=n->next;
+                }
+        return -2;
+}
